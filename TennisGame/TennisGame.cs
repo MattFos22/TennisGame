31dc2ff93e28783b2eee8ea044628194c5159b1a
mattfos22 <mattfoster22@gmail.com>
2014-10-07 11:53:24 +0100

    Added solution from exercise with KR

---
+++ TennisGame/TennisGame.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisGame
+{
+    class TennisGame
+    {
+        private string _currentScore;
+        private static Dictionary<TennisGame, TennisGame[]> _games;
+
+        #region static data structure
+        static TennisGame()
+        {
+            _games = new Dictionary<TennisGame, TennisGame[]> 
+            {
+                {new TennisGame("0-0"), new TennisGame[] {new TennisGame("15-0"), new TennisGame("0-15")}},
+                {new TennisGame("15-0"), new TennisGame[] {new TennisGame("30-0"), new TennisGame("15-15")}},
+                {new TennisGame("30-0"), new TennisGame[] {new TennisGame("40-0"), new TennisGame("30-15")}},
+                {new TennisGame("40-0"), new TennisGame[] {new TennisGame("Player 1 Wins"), new TennisGame("40-15")}},
+                {new TennisGame("0-15"), new TennisGame[] {new TennisGame("15-15"), new TennisGame("0-30")}},
+                {new TennisGame("0-30"), new TennisGame[] {new TennisGame("15-30"), new TennisGame("0-40")}},
+                {new TennisGame("0-40"), new TennisGame[] {new TennisGame("15-40"), new TennisGame("Player 2 Wins")}},
+                {new TennisGame("15-15"), new TennisGame[] {new TennisGame("30-15"), new TennisGame("15-30")}},
+                {new TennisGame("30-15"), new TennisGame[] {new TennisGame("40-15"), new TennisGame("30-30")}},
+                {new TennisGame("40-15"), new TennisGame[] {new TennisGame("Player 1 Wins"), new TennisGame("40-30")}},
+                {new TennisGame("30-30"), new TennisGame[] {new TennisGame("40-30"), new TennisGame("30-40")}},
+                {new TennisGame("30-40"), new TennisGame[] {new TennisGame("Deuce"), new TennisGame("Player 2 Wins")}},
+                {new TennisGame("40-30"), new TennisGame[] {new TennisGame("Player 1 Wins"), new TennisGame("Deuce")}},
+                {new TennisGame("Deuce"), new TennisGame[] {new TennisGame("A-40"), new TennisGame("40-A")}},
+                {new TennisGame("40-A"), new TennisGame[] {new TennisGame("Deuce"), new TennisGame("Player 2 Wins")}},
+                {new TennisGame("A-40"), new TennisGame[] {new TennisGame("Player 1 Wins"), new TennisGame("Deuce")}},
+                {new TennisGame("Player 1 Wins"), new TennisGame[] {new TennisGame("0-0"), new TennisGame("0-0")}},
+                {new TennisGame("Player 2 Wins"), new TennisGame[] {new TennisGame("0-0"), new TennisGame("0-0")}}
+
+            };
+        }
+        #endregion
+
+        public TennisGame(string score)
+        {
+            _currentScore = score;
+        }
+
+        internal string GetCurrentScore()
+        {
+            return _currentScore;
+        }
+
+        internal TennisGame Player1Scores()
+        {
+            var gameOutcomes = _games.Select(game => _games[this]);
+            var value = gameOutcomes.ElementAt(0);
+            return value[0];
+        }
+
+        internal TennisGame Player2Scores()
+        {
+            var gameOutcomes = _games.Where(game => _games.ContainsKey(this)).Select(game => _games[this]);
+            var value = gameOutcomes.ElementAt(1);
+            return value[1];
+        }
+
+        #region overrides
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var game = (TennisGame)obj;
+
+            return game._currentScore == _currentScore;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+           return  _currentScore.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ TennisGame/TennisTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGame
+{
+    [TestClass]
+    public class TennisTests
+    {
+        [TestMethod]
+        public void Should_Return_LoveLove()
+        {
+            var game = new TennisGame("0-0");
+
+            Assert.AreEqual("0-0", game.GetCurrentScore());
+        }
+
+        [TestMethod]
+        public void Should_Return_15Love_When_Player1_Scores()
+        {
+            var game = new TennisGame("0-0");  
+
+            game = game.Player1Scores();
+          
+            Assert.AreEqual("15-0",game.GetCurrentScore());
+        }
+
+        [TestMethod]
+        public void Should_Return_30Love_When_Player1_Scores_Twice()
+        {
+            var game = new TennisGame("0-0");
+
+            game = game.Player1Scores().Player1Scores();
+
+            Assert.AreEqual("30-0", game.GetCurrentScore());
+        }
+
+        [TestMethod]
+        public void Should_Return_3015_When_Player1_Scores_Twice_And_Player2_Scores_Once()
+        {
+            var game = new TennisGame("0-0");
+
+            game = game.Player1Scores().Player1Scores().Player2Scores();
+
+            Assert.AreEqual("30-15", game.GetCurrentScore());
+        }
+
+    
+    }
+}
